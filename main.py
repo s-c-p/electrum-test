23468964ff8478a11f457f502ed109098e35e005
s-c-p <prasadsatish45@gmail.com>
2022-02-19 13:30:08 +0530

    updates to welcome new db backend

--- main.py
+++ main.py
@@ -3,30 +3,27 @@ from typing import List, Optional
 
 from fastapi import FastAPI, Request
 
-from ds import Movie
-from db import load_database
-from utils import find_movie, find_all_movies
+from db import Movie, find_all_movies, find_movie, create_db_tables_and_testdata
 
 app = FastAPI()
-
-all_movies = load_database()
+create_db_tables_and_testdata()
 
 @app.get("/searchOne/t/{title}")
 def get_movie_by_id(title: str):
-	return find_movie("title", title, all_movies)
+	return find_movie("title", title)
 
 @app.get("/searchOne/id/{movie_id}")
 def get_movie_by_id(movie_id: str):
-	return find_movie("m_id", movie_id, all_movies)
+	return find_movie("m_id", movie_id)
 
 @app.get("/searchAll/rating/{r}")
 def get_movies_by_rating(r: float):
-	return find_all_movies("rating", r, all_movies)
+	return find_all_movies("rating", r)
 
 @app.get("/searchAll/genere/{g}")
 def get_movies_by_genere(g: str):
-	return find_all_movies("genere", g, all_movies)
+	return find_all_movies("genere", g)
 
 @app.get("/searchAll/year/{y}")
 def get_movies_by_year(y: int):
-	return find_all_movies("release_year", y, all_movies)
+	return find_all_movies("release_year", y)
